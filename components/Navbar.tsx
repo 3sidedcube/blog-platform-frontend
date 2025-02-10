a653721f15e1736b80d7de96d2587e1df09b92b7
michealsided <micheal@3sidedcube.com>
2025-02-10 10:23:18 +0000

    fix logout route

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,23 @@
 "use client";
 import Link from 'next/link';
 import { useAuth } from '../hooks/useAuth';
+import { Router } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 export default function Navbar() {
+  const router = useRouter()
   const { isAuthenticated, logout } = useAuth();
-
+ const handleLogout = ()=>{
+     logout()
+     router.push("/account/login")
+  }
   return (
     <nav className="bg-gray-800 text-white p-4 flex justify-between">
       <Link href="/" className="text-xl font-bold">My Blog</Link>
 
       <div className="flex space-x-4">
         {isAuthenticated ? (
-          <><button onClick={logout} className="bg-red-500 px-4 py-2 rounded">Logout</button>
+          <><button onClick={handleLogout} className="bg-red-500 px-4 py-2 rounded">Logout</button>
           <Link href="/post/create" className="bg-blue-500 px-4 py-2 rounded">Create Post</Link>
           <Link href="/post/myposts" className="bg-blue-500 px-4 py-2 rounded">My Posts</Link>
           </>
--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,29 +1,7 @@
 "use client";
-import { useState, useEffect, useContext,createContext } from 'react';
 
-interface AuthContextType{
-  isAuthenticated : boolean
-  login: (token:string)=> void
-  logout: ()=> void
-}
-export const AuthContext = createContext<AuthContextType | null>(null);
-export  function AuthProvider ({children}: {children:React.ReactNode}){
-  
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
-  useEffect(()=>{
-    const token = localStorage.getItem("access_token")
-    setIsAuthenticated(!!token)
-  }, [])
-  const login = (token:string)=>{
-    localStorage.setItem("access_token",token)
-    setIsAuthenticated(true)
-  }
-  const logout = ()=>{
-    localStorage.removeItem("access_token")
-    setIsAuthenticated(false)
-  }
-  //return(<AuthContext>)
-}
+import { useEffect, useState } from "react";
+
 export function useAuth() {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
 
